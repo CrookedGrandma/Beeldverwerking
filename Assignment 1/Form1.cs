2398180fc9b56a5be65be0d6a9bdb6cd3099d889
CrookedGrandma <k.t.noot@live.nl>
2018-09-21 23:52:02 +0200

    HOLD UP STOP HET DOET DINGEN DIT MOETEN WE OPSLAAN

--- Assignment 1/Form1.cs
+++ Assignment 1/Form1.cs
@@ -8,22 +8,18 @@ using System.Text;
 using System.Windows.Forms;
 using System.IO;
 
-namespace INFOIBV
-{
-    public partial class INFOIBV : Form
-    {
+namespace INFOIBV {
+    public partial class INFOIBV : Form {
         private Bitmap InputImage;
         private Bitmap OutputImage;
 
-        public INFOIBV()
-        {
+        public INFOIBV() {
             InitializeComponent();
         }
 
-        private void LoadImageButton_Click(object sender, EventArgs e)
-        {
-           if (openImageDialog.ShowDialog() == DialogResult.OK)             // Open File Dialog
-            {
+        private void LoadImageButton_Click(object sender, EventArgs e) {
+            if (openImageDialog.ShowDialog() == DialogResult.OK)             // Open File Dialog
+             {
                 string file = openImageDialog.FileName;                     // Get the file name
                 imageFileName.Text = file;                                  // Show file name
                 if (InputImage != null) InputImage.Dispose();               // Reset image
@@ -32,22 +28,19 @@ namespace INFOIBV
                     InputImage.Size.Height > 512 || InputImage.Size.Width > 512) // Dimension check
                     MessageBox.Show("Error in image dimensions (have to be > 0 and <= 512)");
                 else
-                    pictureBox1.Image = (Image) InputImage;                 // Display input image
+                    pictureBox1.Image = (Image)InputImage;                 // Display input image
             }
         }
 
-        private void applyButton_Click(object sender, EventArgs e)
-        {
+        private void applyButton_Click(object sender, EventArgs e) {
             if (InputImage == null) return;                                 // Get out if no input image
             if (OutputImage != null) OutputImage.Dispose();                 // Reset output image
             OutputImage = new Bitmap(InputImage.Size.Width, InputImage.Size.Height); // Create new output image
             Color[,] image = new Color[InputImage.Size.Width, InputImage.Size.Height]; // Create array to speed-up operations (Bitmap functions are very slow)
 
             // Copy input Bitmap to array            
-            for (int x = 0; x < InputImage.Size.Width; x++)
-            {
-                for (int y = 0; y < InputImage.Size.Height; y++)
-                {
+            for (int x = 0; x < InputImage.Size.Width; x++) {
+                for (int y = 0; y < InputImage.Size.Height; y++) {
                     image[x, y] = InputImage.GetPixel(x, y);                // Set pixel color in array at (x,y)
                 }
             }
@@ -60,24 +53,26 @@ namespace INFOIBV
             //Grayscale(image);
 
             // (2) Contrast
-            Contrast(image);
-            
+            //Contrast(image);
+
+            float[,] yeet = GaussianKernel(3, 1);
+
+            // (4) Linear Filter
+            Linear(image, UniformKernel(5));
+
             //==========================================================================================
 
             // Copy array to output Bitmap
-            for (int x = 0; x < InputImage.Size.Width; x++)
-            {
-                for (int y = 0; y < InputImage.Size.Height; y++)
-                {
+            for (int x = 0; x < InputImage.Size.Width; x++) {
+                for (int y = 0; y < InputImage.Size.Height; y++) {
                     OutputImage.SetPixel(x, y, image[x, y]);               // Set the pixel color at coordinate (x,y)
                 }
             }
-            
+
             pictureBox2.Image = (Image)OutputImage;                         // Display output image
         }
-        
-        private void saveButton_Click(object sender, EventArgs e)
-        {
+
+        private void saveButton_Click(object sender, EventArgs e) {
             if (OutputImage == null) return;                                // Get out if no output image
             if (saveImageDialog.ShowDialog() == DialogResult.OK)
                 OutputImage.Save(saveImageDialog.FileName);                 // Save the output image
@@ -145,6 +140,70 @@ namespace INFOIBV
                     image[x, y] = updatedColor;
                 }
             }
+        }
+
+        private void Linear(Color[,] image, float[,] matrix) {
+            int size = matrix.GetLength(0);
+            int radius = size / 2;
+            int width = InputImage.Size.Width;
+            int height = InputImage.Size.Height;
+            int xrange = width - 1;
+            int yrange = height - 1;
+            float sum = matrix.Cast<float>().Sum();
+            Color[,] database = (Color[,])image.Clone();
+            for (int x = 0; x < width; x++) {
+                for (int y = 0; y < height; y++) {
+                    float output = 0;
+                    for (int u = x - radius; u <= x + radius; u++) {
+                        for (int v = y - radius; v <= y + radius; v++) {
+                            int eu = Math.Abs(-Math.Abs(u - xrange) + xrange);
+                            int ev = Math.Abs(-Math.Abs(v - yrange) + yrange); //mirror at edges
+                            output += database[eu, ev].R * matrix[u - x + radius, v - y + radius];
+                        }
+                    }
+                    int op = (int)output;
+                    image[x, y] = Color.FromArgb(op, op, op);
+                }
+            }
+        }
+
+        // Kernel functions
+
+        private float[,] UniformKernel(int size) {
+            if (size % 2 < 1) throw new Exception("Kernel size not an odd number.");
+            float[,] kernel = new float[size, size];
+            for (int x = 0; x < size; x++) {
+                for (int y = 0; y < size; y++) {
+                    kernel[x, y] = 1 / (float)(size * size);
+                }
+            }
+            return kernel;
+        }
+
+        private float[,] GaussianKernel(int size, float sigma) {
+            if (size % 2 < 1) throw new Exception("Kernel size not an odd number.");
+            float[,] kernel = new float[size, size];
+            float sum = 0;
+            for (int x = 0; x < size; x++) {
+                for (int y = 0; y < size; y++) {
+                    float dist = (float)Math.Sqrt((x - (size / 2)) * (x - (size / 2)) + (y - (size / 2)) * (y - (size / 2)));
+                    float val = NormalPDF(sigma, dist);
+                    kernel[x, y] = val;
+                    sum += val;
+                }
+            }
+            for (int x = 0; x < size; x++) {
+                for (int y = 0; y < size; y++) {
+                    kernel[x, y] /= sum;
+                }
+            }
+            return kernel;
+        }
+
+        // Other supportive functions
+
+        private float NormalPDF(float sigma, float x) {
+            return (float)(1 / (sigma * Math.Sqrt(2 * Math.PI)) * Math.Exp(-(x * x) / (2 * sigma * sigma)));
         }
 
         //==============================================================================================
